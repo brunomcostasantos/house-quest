893f95d58861ba2acfd90b946fcf05914b693cc3
Bruno Santos <brunom.dacostasantos@gmail.com>
2026-02-02 16:33:05 +0000

    Initial Spring Boot project generated via Spring Initializr

---
+++ src/main/java/com/warple/house_quest/HouseQuestApplication.java
@@ -0,0 +1,13 @@
+package com.warple.house_quest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouseQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouseQuestApplication.class, args);
+	}
+
+}
